c3724a70a58444ed4c11c42ce9ffa2c0ef81b7fc
Serhii Severin <sseverin@cybervisiontech.com>
2023-03-19 16:57:33 +0200

    Moved Main.java to the demo package

--- src/main/java/com/breskul/demo/Main.java
+++ src/main/java/com/breskul/demo/Main.java
@@ -1,11 +1,16 @@
-package com.breskul.bibernate;
+package com.breskul.demo;
 
+import com.breskul.bibernate.repository.DataSourceFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
+import javax.sql.DataSource;
+
 public class Main {
     public static void main(String[] args) {
+        DataSource dataSource = DataSourceFactory.getInstance().getDataSource();
+
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("demo");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
         entityManagerFactory.close();
