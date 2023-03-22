0543fe4c0a4295424e27d7c81d557885f9e4adb2
Serhii Severin <sseverin@cybervisiontech.com>
2023-03-22 19:03:13 +0200

    Updated PersistencePropertiesException

--- src/main/java/com/breskul/bibernate/exeptions/PersistencePropertiesException.java
+++ src/main/java/com/breskul/bibernate/exeptions/PersistencePropertiesException.java
@@ -6,17 +6,17 @@ package com.breskul.bibernate.exeptions;
  */
 public class PersistencePropertiesException extends CommonException {
 
-    private static final String CANT_FIND_CAUSE = "Unable to find '%s' file";
-    private static final String CANT_FIND_SUGGESTION = "The 'persistence.properties' file should be in the classpath";
+    private static final String CAN_NOT_FIND_CAUSE = "Unable to find '%s' file";
+    private static final String CAN_NOT_FIND_SUGGESTION = "The '%s' file should be in the classpath";
 
-    private static final String CANT_LOAD_CAUSE = "Unable to load '%s' file";
-    private static final String CANT_LOAD_SUGGESTION = "The 'persistence.properties' file should be the correct Properties file";
+    private static final String CAN_NOT_LOAD_CAUSE = "Unable to load '%s' file";
+    private static final String CAN_NOT_LOAD_SUGGESTION = "The '%s' file should be the correct Properties file";
 
     public PersistencePropertiesException(String propertiesFile) {
-        super(String.format(CANT_FIND_CAUSE, propertiesFile), CANT_FIND_SUGGESTION);
+        super(String.format(CAN_NOT_FIND_CAUSE, propertiesFile), String.format(CAN_NOT_FIND_SUGGESTION, propertiesFile));
     }
 
     public PersistencePropertiesException(String propertiesFile, Throwable e) {
-        super(String.format(CANT_LOAD_CAUSE, propertiesFile), CANT_LOAD_SUGGESTION, e);
+        super(String.format(CAN_NOT_LOAD_CAUSE, propertiesFile), String.format(CAN_NOT_LOAD_SUGGESTION, propertiesFile), e);
     }
 }
