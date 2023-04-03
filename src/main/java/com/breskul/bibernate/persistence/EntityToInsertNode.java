7f5eb569a774f4d46ce20501a58f8d2d6180cb93
Yuriy Fomin <yuriy.fomin@coro.net>
2023-04-03 20:27:57 +0100

    EntityToInsertNode documentation -added documentation

---
+++ src/main/java/com/breskul/bibernate/persistence/EntityToInsertNode.java
@@ -0,0 +1,13 @@
+package com.breskul.bibernate.persistence;
+
+import com.breskul.bibernate.annotation.OneToMany;
+
+import java.util.List;
+
+/**
+ * <h3>{@link EntityToInsertNode} provides an abstraction for entity. In case parent entity has collection field {@link OneToMany}, it is easier to think of it as a tree with parent node and childes nodes  </h3>
+ * @param entity {@link Object} entity to be inserted
+ * @param childes {@link List} list of child nodes to be inserted in database
+ */
+public record EntityToInsertNode(Object entity, List<EntityToInsertNode> childes) {
+}
