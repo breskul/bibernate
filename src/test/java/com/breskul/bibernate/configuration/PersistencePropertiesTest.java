d4c6cc0d1ab33288fe0328122d324a5cf62c71d6
Gleb Nogai <Gleb.Nogai@nordstrom.com>
2023-04-08 12:00:34 +0300

    Update setup action

--- src/test/java/com/breskul/bibernate/configuration/PersistencePropertiesTest.java
+++ src/test/java/com/breskul/bibernate/configuration/PersistencePropertiesTest.java
@@ -9,36 +9,36 @@ import java.lang.reflect.Field;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PersistencePropertiesTest {
-//    @AfterEach
-//    void nullifyInstance() {
-//        PersistenceProperties.clear();
-//    }
-//
-//    @Test
-//    @DisplayName("Test getProperty")
-//    void testGetProperty() {
-//        PersistenceProperties.initialize();
-//        PersistenceProperties persistenceProperties = PersistenceProperties.getInstance();
-//        String value = persistenceProperties.getProperty("db.url");
-//        assertEquals("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;INIT=runscript from 'classpath:/sql/init.sql'", value);
-//    }
-//
-//    @Test
-//    @DisplayName("Test initialize with custom file")
-//    void testInitializeCustomFile() {
-//        PersistenceProperties.initialize("custom.properties");
-//        PersistenceProperties persistenceProperties = PersistenceProperties.getInstance();
-//
-//        String value = persistenceProperties.getProperty("db.url");
-//        assertEquals("jdbc:postgresql://localhost:5432/postgres", value);
-//    }
-//
-//    @Test
-//    @DisplayName("Test load properties without initialization")
-//    void testLoadPropertiesWithoutInitialize() {
-//        assertThrows(InitializePersistencePropertiesException.class,
-//                PersistenceProperties::getInstance);
-//    }
+    @BeforeEach
+    void nullifyInstance() {
+        PersistenceProperties.clear();
+    }
+
+    @Test
+    @DisplayName("Test getProperty")
+    void testGetProperty() {
+        PersistenceProperties.initialize();
+        PersistenceProperties persistenceProperties = PersistenceProperties.getInstance();
+        String value = persistenceProperties.getProperty("db.url");
+        assertEquals("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;INIT=runscript from 'classpath:/sql/init.sql'", value);
+    }
+
+    @Test
+    @DisplayName("Test initialize with custom file")
+    void testInitializeCustomFile() {
+        PersistenceProperties.initialize("custom.properties");
+        PersistenceProperties persistenceProperties = PersistenceProperties.getInstance();
+
+        String value = persistenceProperties.getProperty("db.url");
+        assertEquals("jdbc:postgresql://localhost:5432/postgres", value);
+    }
+
+    @Test
+    @DisplayName("Test load properties without initialization")
+    void testLoadPropertiesWithoutInitialize() {
+        assertThrows(InitializePersistencePropertiesException.class,
+                PersistenceProperties::getInstance);
+    }
 
     @Test
     @DisplayName("Test load properties not found")
